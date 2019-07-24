a5c1be71b966f8f5119553a992dca815e1ceffd3
Yura Oleksyshyn <yuraolex@gmail.com>
2019-07-24 15:17:56 +0300

    Added metadata setting

--- rpcclient/grpcclient/grpcclient.go
+++ rpcclient/grpcclient/grpcclient.go
@@ -1,11 +1,20 @@
 package grpcclient
 
 import (
-
+	"context"
 	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/btcsuite/btcd/rpcclient/grpcclient/protos"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
+
+
+// unaryInterceptor is an example unary interceptor.
+func interceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+
+	ctx = metadata.AppendToOutgoingContext(ctx, "assetid", "0")
+	return invoker(ctx, method, req, reply, cc, opts...)
+}
 
 // New creates a new RPC client based on the provided connection configuration
 // details.  The notification handlers parameter may be nil if you are not
@@ -13,7 +22,7 @@ import (
 // configuration is set to run in HTTP POST mode.
 func New(config *rpcclient.ConnConfig) (lightwalletrpc.LightWalletServiceClient, error) {
 
-	clientConn, err := grpc.Dial(config.Host, grpc.WithInsecure())
+	clientConn, err := grpc.Dial(config.Host, grpc.WithInsecure(), grpc.WithUnaryInterceptor(interceptor))
 	if err != nil {
 		return nil, err
 	}
